7756ad833000b95ba2553d84abe968a8d5ee716c
Rachel Daniel <rachdaniel88@gmail.com>
2018-12-05 11:33:19 -0600

    set up top level components

---
+++ src/GiftStorm.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react"
+import NavBar from "./nav/NavBar"
+import App from "./App"
+
+export default class GiftStorm extends Component {
+
+  render() {
+      return (
+          <React.Fragment>
+              <NavBar />
+              <App />
+          </React.Fragment>
+      )
+  }
+}
