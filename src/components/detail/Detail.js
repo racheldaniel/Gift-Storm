a8278144639b536911a64490e46108b8dca8e273
Rachel Daniel <rachdaniel88@gmail.com>
2018-12-08 14:42:52 -0600

    added routing from list to details, live data for interests and gift ideas

--- src/components/detail/Detail.js
+++ src/components/detail/Detail.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {
-  ListGroup, Container, Card, Row, Col, ListGroupItem, CardText, CardDeck,
-  CardBody, Badge, CardHeader, ListGroupItemHeading, FormGroup, Label, Input
+  ListGroup, Container, Card, CardText, CardDeck,
+  CardBody, CardHeader
 } from 'reactstrap';
 
 import DetailInterests from "./DetailInterests"
@@ -9,41 +9,97 @@ import DetailGifts from "./DetailGifts"
 import DetailCelebrations from "./DetailCelebrations"
 
 import "./Detail.css"
+import API from "./../../modules/API/API"
+
 
 export default class FriendDetail extends Component {
+  state = {
+    friendOccGifts: [],
+    occasions: [],
+    friendDetail: [],
+    isLoaded: false
+  }
+  //function fetches this friend's occasions and corresponding gifts with user-occasion entity embedded. This can be used to find image from occasion array
+  findFriendGifts = () => {
+    const friend = this.props.friends.find(a => a.id === parseInt(this.props.match.params.friendId)) || {}
+    return API.getData(`friend_occasions?friendId=${friend.id}&_embed=gifts&_expand=user_occasion`)
+      .then((friendOccGifts) => this.setState({ friendOccGifts: friendOccGifts, isLoaded: true }))
+  }
+
+  findFriend = () => {
+    const friend = this.props.friends.find(a => a.id === parseInt(this.props.match.params.friendId)) || {}
+    return API.getData(`friends?id=${friend.id}`)
+      .then((friendDetail) => this.setState({ friendDetail: friendDetail }))
+  }
+
+  findOccasions = () => {
+    return API.getData(`occasions`)
+      .then((occasions) => this.setState({ occasions: occasions }))
+  }
+
+  componentDidMount() {
+    this.findOccasions()
+      .then(() => this.findFriend())
+      .then(() => this.findFriendGifts())
+  }
+
   render() {
+    const friend = this.props.friends.find(a => a.id === parseInt(this.props.match.params.friendId)) || {}
+
     return (
-      <Container>
-        <h1 className="text-center text-light my-5">Hannah's GiftStorm</h1>
-        <CardDeck>
-          <Card className="detailCard">
-            <CardBody>
-              <CardHeader className="detailCard--Heading text-center">Interests</CardHeader>
-              <CardText className=" d-flex align-items-center flex-wrap text-center">
-                <DetailInterests />
+      <React.Fragment>
+        {
+          (this.state.isLoaded === true)
+            ? <Container>
+              <h1 className="text-center text-light my-5">{`${friend.name}'s Giftstorm`}</h1>
+              <CardDeck>
+                <Card className="detailCard">
+                  <CardBody>
+                    <CardHeader className="detailCard--Heading text-center">Interests</CardHeader>
+                    <CardText className=" d-flex align-items-center flex-wrap text-center">
+                      {
+                        this.state.friendDetail[0].interests.map(interest =>
+                          <DetailInterests
+                            interest={interest}
+                            friendDetail={this.state.friendDetail}
+                          />
+                        )
+                      }
 
-              </CardText>
-              <i className="icon-plus float-right "></i>
-            </CardBody>
-          </Card>
 
-          <Card className="detailCard">
-            <CardBody>
-              <CardHeader className="detailCard--Heading text-center">Gift Ideas</CardHeader>
-              <CardText className=" d-flex align-items-center flex-wrap">
-                <ListGroup className="detailCard--ul">
-                  <DetailGifts />
-                </ListGroup>
-              </CardText>
-              <i className="icon-plus float-right "></i>
-            </CardBody>
-          </Card>
-        </CardDeck>
-        <ListGroup className="mt-5">
-          <h3 className="text-center text-light">Celebrations</h3>
-          <DetailCelebrations />
-        </ListGroup>
-      </Container>
+                    </CardText>
+                    <i className="icon-plus float-right "></i>
+                  </CardBody>
+                </Card>
+
+                <Card className="detailCard">
+                  <CardBody>
+                    <CardHeader className="detailCard--Heading text-center">Gift Ideas</CardHeader>
+                    <CardText className=" d-flex align-items-center flex-wrap">
+                      <ListGroup className="detailCard--ul">
+                        {
+                          this.state.friendDetail[0].giftIdeas.map(giftIdea =>
+                            <DetailGifts
+                              giftIdea={giftIdea}
+                              friendDetail={this.state.friendDetail}
+                            />
+                          )
+                        }
+                      </ListGroup>
+                    </CardText>
+                    <i className="icon-plus float-right "></i>
+                  </CardBody>
+                </Card>
+              </CardDeck>
+              <ListGroup className="mt-5">
+                <h3 className="text-center text-light">Celebrations</h3>
+                <DetailCelebrations />
+              </ListGroup>
+            </Container>
+            : null
+        }
+      </React.Fragment>
+
     );
   }
 }
--- src/components/detail/DetailInterests.js
+++ src/components/detail/DetailInterests.js
@@ -6,15 +6,7 @@ export default class DetailInterests extends Component {
   render() {
     return (
       <React.Fragment>
-        <Badge className="mx-3 my-3 pill" color="warning" pill>Architecture</Badge>
-        <Badge className="mx-3 my-3 pill" color="warning" pill>Norah</Badge>
-        <Badge className="mx-3 my-3 pill" color="warning" pill>Needlepoint</Badge>
-        <Badge className="mx-3 my-3 pill large" color="warning" pill>Hiking</Badge>
-        <Badge className="mx-3 my-3 pill" color="warning" pill>Camping</Badge>
-        <Badge className="mx-3 my-3 pill" color="warning" pill>Climbing</Badge>
-        <Badge className="mx-3 my-3 pill large" color="warning" pill>Harry Potter</Badge>
-        <Badge className="mx-3 my-3 pill" color="warning" pill>Reading</Badge>
-        <Badge className="mx-3 my-3 pill" color="warning" pill>Running</Badge>
+        <Badge className="mx-3 my-3 pill" color="warning" pill>{this.props.interest}</Badge>
       </React.Fragment>
 
     )
