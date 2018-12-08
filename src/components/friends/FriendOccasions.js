01385545b06c211b1f307a353c0ca001f7a1733c
Rachel Daniel <rachdaniel88@gmail.com>
2018-12-08 13:44:47 -0600

    added live data to friend list

---
+++ src/components/friends/FriendOccasions.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { ListGroupItemText, Col } from 'reactstrap';
+
+
+export default class FriendOccasions extends Component {
+
+
+  render() {
+
+    return (
+      <React.Fragment>
+        <Col className="text-center" >
+          <img src={this.props.userOccasions.find(occ =>
+            occ.occasionId === this.props.friend_occasion.user_occasionId
+          ).occasion.img}
+
+          alt="christmas" className="img-thumbnail" />
+
+          <ListGroupItemText>{this.props.friend_occasion.date}</ListGroupItemText>
+        </Col>
+      </React.Fragment>
+
+    )
+  }
+}
